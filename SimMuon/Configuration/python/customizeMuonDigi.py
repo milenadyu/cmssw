001e02c81a9ed97b6f3636b379be2ac1189b3b72
Junghwan John Goh <jhgoh@cern.ch>
2017-05-17 02:30:37 +0200

    modify rpcRedigi customization function to be less-verbatim

--- SimMuon/Configuration/python/customizeMuonDigi.py
+++ SimMuon/Configuration/python/customizeMuonDigi.py
@@ -302,43 +302,18 @@ def customize_digi_noRPCbkg(process):
 def customise_rpcRedigi(process):
     process.load('Configuration.StandardSequences.Digi_cff')
     process.simMuonRPCReDigis = process.simMuonRPCDigis.clone()
-    process.simMuonRPCReDigis.digiRPCModelConfig = cms.PSet(
-        Frate = cms.double(1.0),
-        Gate = cms.double(1.0),
+    process.simMuonRPCReDigis.digiModelConfig = process.simMuonRPCDigis.digiModelConfig.clone(
+        IRPC_time_resolution = cms.double(1.5),
         IRPC_electronics_jitter = cms.double(0.1),
-        IRPC_time_resolution = cms.double(1.0),
+        timingRPCOffset = cms.double(50.0),
         Nbxing = cms.int32(799),
         BX_range = cms.int32(400),
-        Rate = cms.double(0.0),
-        averageClusterSize = cms.double(1.5),
-        averageEfficiency = cms.double(0.95),
-        cosmics = cms.bool(False),
-        deltatimeAdjacentStrip = cms.double(3.0),
         linkGateWidth = cms.double(1.0),
-        printOutDigitizer = cms.bool(False),
-        signalPropagationSpeed = cms.double(0.66),
-        timeJitter = cms.double(1.0),
-        timeResolution = cms.double(2.5),
-        timingRPCOffset = cms.double(50.0)
-    ),
-    process.simMuonRPCReDigis.digiIRPCModelConfig = cms.PSet(
-        Frate = cms.double(1.0),
-        Gate = cms.double(1.0),
-        IRPC_electronics_jitter = cms.double(0.1),
+    )
+    process.simMuonRPCReDigis.digiIRPCModelConfig = process.simMuonRPCReDigis.digiModelConfig.clone(
         IRPC_time_resolution = cms.double(1.0),
-        Nbxing = cms.int32(799),
-        BX_range = cms.int32(400),
-        Rate = cms.double(0.0),
-        averageClusterSize = cms.double(1.5),
-        averageEfficiency = cms.double(0.95),
-        cosmics = cms.bool(False),
-        deltatimeAdjacentStrip = cms.double(3.0),
-        linkGateWidth = cms.double(1.0),
-        printOutDigitizer = cms.bool(False),
-        signalPropagationSpeed = cms.double(0.66),
-        timeJitter = cms.double(1.0),
-        timeResolution = cms.double(2.5),
-        timingRPCOffset = cms.double(50.0)
+        IRPC_electronics_jitter = cms.double(0.1),
+        timeResolution = cms.double(1.0),
     )
     process.RandomNumberGeneratorService.simMuonRPCReDigis = cms.PSet(
         initialSeed = cms.untracked.uint32(13579),
